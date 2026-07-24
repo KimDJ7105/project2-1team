d68ea28ae78d3a696c8f1d3d6846084cdf6bdb05
Kim dong jae <rlaehdwo1225@naver.com>
2026-07-24 12:00:57 +0900

    fix: dev일때만  env 파일을 읽어오게 수정

--- server/src/config/configLoader.ts
+++ server/src/config/configLoader.ts
@@ -3,7 +3,9 @@ import path from 'path';
 import yaml from 'js-yaml';
 import dotenv from 'dotenv';
 
-dotenv.config({ path: path.join(__dirname, '../.env') });
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config({ path: path.join(__dirname, '../.env') });
+}
 
 export interface ClientConfig {
   url: string;
