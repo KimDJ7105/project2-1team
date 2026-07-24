03602e04af3677efb7b84c02036c750cb935392f
Jaden Yang <jhy0787@gmail.com>
2026-07-24 10:33:58 +0900

    feat: IRSA 역할 ARN 및 클러스터 엔드포인트 출력값 추가

---
+++ infra/terraform/outputs.tf
@@ -0,0 +1,11 @@
+# terraform apply 완료 후 콘솔에 출력할 값들
+
+output "backend_irsa_role_arn" {
+  description = "team1-backend-sa가 사용할 IAM 역할 ARN"
+  value       = module.backend_irsa.iam_role_arn
+}
+
+output "cluster_endpoint" {
+  description = "EKS 클러스터 API 서버 주소"
+  value       = module.eks.cluster_endpoint
+}
